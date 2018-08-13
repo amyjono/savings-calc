b740ebffd73a33317b7f34b87e613d8a3a6eff8d
Amy Johnson <amylouise_johnson@hotmail.com>
2018-08-13 13:45:39 +0100

    added error message for isNaN entries and fixed loop so that blank fields equal 0 and have no error.

--- js/main-js.js
+++ js/main-js.js
@@ -1,62 +1,85 @@
-$(document).ready(function () { 
-    
+$(document).ready(function () {
+
     calculate = function () {
-        
-        // Collects object arrays from ".income" sections
-        var getIncomeList = document.getElementsByClassName("income");
-        
+
+        // Object arrays from ".income" sections
+        var getIncomeList = $(".income");
+
         // Empty array ready for income values to be added
         var income = [];
-        
-        // Loop to add values from object arrays into var income
-        for (var i = 0; i < getIncomeList.length; i ++ ) {
-        // Pushes values to array
+
+        // Selects .incomeErr classes incase an error message is needed
+        var incomeMsg = $(".incomeErr");
+
+        // Loop to add values from income object arrays into var income
+        for (var i = 0; i < getIncomeList.length; i++) {
+            // Pushes values to income array
             income.push($(getIncomeList[i]).val());
-        // Converts inputs to int
-            if (isNaN(income[i]) === true) {
+    
+            if (isNaN(income[i])) {
+                // Error message for isNaN entries
+                incomeMsg[i].innerHTML = "Please enter a valid number!";
+                income[i] = 0;
+            } else if (income[i] == "") {
+                // Fields left blank are made === 0
                 income[i] = 0;
             } else {
+                // Converts to int
                 income[i] = parseFloat(income[i], 10);
+                // Removes error message on submit
+                incomeMsg[i].innerHTML = "";
             }
-        };
-              
+        }
+
         // Adds together values from income array
-        addValues = function(items){
-            return items.reduce( function(a, b) {
+        addValues = function (items) {
+            return items.reduce(function (a, b) {
                 return a + b;
             });
         }
-      
+
         var incomeTotal = addValues(income);
-        
-        // Collects object arrays from ".expenses" sections
-        var getExpensesList = document.getElementsByClassName("expenses");
-        
-        // Empty array ready for income values to be added
+
+        // Object arrays from ".expenses" sections
+        var getExpensesList = $(".expenses");
+
+        // Empty array ready for expenses values to be added
         var expenses = [];
-        
-        // Loop to add values from object arrays into var income
-        for (var e = 0; e < getExpensesList.length; e ++) {
-        // Pushes values to array
-            expenses.push($(getExpensesList[e]).val());
-        // Converts inputs to int
-            if (isNaN(expenses[e]) === true) {
-                expenses[e] = 0;
+
+        // Selects .expensesErr classes incase an error message is needed
+        var expensesMsg = $(".expensesErr");
+
+        // Loop to add values from expenses object arrays into var expenses
+        for (var i = 0; i < getExpensesList.length; i++) {
+            // Pushes values to expenses array
+            expenses.push($(getExpensesList[i]).val());
+
+            if (isNaN(expenses[i])) {
+                // Error message for isNaN entries
+                expensesMsg[i].innerHTML = "Please enter a valid number!";
+                expenses[i] = 0;
+            } else if (expenses[i] == "") {
+                // Fields left blank are made === 0
+                expenses[i] = 0;
             } else {
-                expenses[e] = parseFloat(expenses[e], 10);
+                // Converts to int
+                expenses[i] = parseFloat(expenses[i], 10);
+                // Removes error message on submit
+                expensesMsg[i].innerHTML = "";
             }
         }
-    
+
         var expensesTotal = addValues(expenses);
-        
+
         // Display total in browser
         document.getElementById('total').value = incomeTotal - expensesTotal.toFixed(2);
 
     };
-    
+
+
     $("#button").hover(function () {
         $(this).css("background-color", "#4985e5"), $(this).css("color", "#192868"), $(this).css("border", "#192868 4px solid");
-        }, function(){
+    }, function () {
         $(this).css("background-color", "#192868"), $(this).css("color", "#b5dcff"), $(this).css("border", "#4985e5 4px solid");
     });
 });
